e9f31fddd763e9b4873b620ab328262d3f79ecde
Bakr <bakr22hj@gmail.com>
2024-03-08 00:43:01 -0500

    Modify Basic form and add questions

--- FrontendCalculator/src/Forms/BasicForm.js
+++ FrontendCalculator/src/Forms/BasicForm.js
@@ -12,26 +12,35 @@ import {
   Typography,
   FormHelperText,
 } from "@mui/material";
+import { faL } from "@fortawesome/free-solid-svg-icons";
 
 const BasicForm = () => {
   const navigate = useNavigate(); // Initialize navigate function
 
   const [formData, setFormData] = useState({
     computeComplexity: "",
-    networkReliability: "",
-    dataStorageSize: "",
+    // networkReliability: "",
+    dataStorageType: "",
     databaseService: "",
     monthlyBudget: "",
-    resourceGrowth: "",
+    expectedUsers: "",
+    dnsFeature: "",
+    cdnNetworking: "",
+    region: "",
+    // resourceGrowth: "",
   });
 
   const [validationErrors, setValidationErrors] = useState({
     computeComplexity: false,
-    networkReliability: false,
-    dataStorageSize: false,
+    // networkReliability: false,
+    dataStorageType: false,
     databaseService: false,
     monthlyBudget: false,
-    resourceGrowth: false,
+    expectedUsers: false,
+    dnsFeature: false,
+    cdnNetworking: false,
+    region: false,
+    // resourceGrowth: false,
   });
 
   const handleChange = (event) => {
@@ -87,11 +96,16 @@ const BasicForm = () => {
     const timer = setTimeout(() => {
       setValidationErrors({
         computeComplexity: false,
-        networkReliability: false,
-        dataStorageSize: false,
+        // networkReliability: false,
+        dataStorageType: false,
         databaseService: false,
         monthlyBudget: false,
-        resourceGrowth: false,
+        expectedUsers: false,
+        dnsFeature: false,
+        cdnNetworking: false,
+        region: false,
+    
+        // resourceGrowth: false,
       });
     }, 3000);
     return () => clearTimeout(timer);
@@ -119,15 +133,14 @@ const BasicForm = () => {
                   error={validationErrors.computeComplexity}
                 >
                   <MenuItem value="">Select...</MenuItem>
-                  <MenuItem value="simple">
-                    Simple: Basic computing tasks with minimal processing requirements
+                  <MenuItem value="basic">
+                  Basic Computing
                   </MenuItem>
                   <MenuItem value="moderate">
-                    Moderate: Moderate computing tasks with occasional spikes in processing needs
+                    Moderate
                   </MenuItem>
-                  <MenuItem value="complex">
-                    Complex: Intensive computing tasks with high processing demands and complex
-                    algorithms
+                  <MenuItem value="intensive">
+                  Intensive
                   </MenuItem>
                 </Select>
                 {validationErrors.computeComplexity && (
@@ -135,8 +148,36 @@ const BasicForm = () => {
                 )}
               </FormControl>
             </Grid>
-
-            {/* Network Reliability */}
+            {/* Expected Users */}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel id="expected-users-label">Expected Users</InputLabel>
+                <Select
+                  labelId="expected-users-label"
+                  id="expected-users-select"
+                  value={formData.expectedUsers}
+                  onChange={handleChange}
+                  label="Expected Users"
+                  name="expectedUsers"
+                  error={validationErrors.expectedUsers}
+                >
+                  <MenuItem value="">Select...</MenuItem>
+                  <MenuItem value="1000">
+                    Less than 1000
+                  </MenuItem>
+                  <MenuItem value="5000">
+                    5000
+                  </MenuItem>
+                  <MenuItem value="10000+">
+                    10000+
+                  </MenuItem>
+                </Select>
+                {validationErrors.expectedUsers && (
+                  <FormHelperText error>Please select expected users</FormHelperText>
+                )}
+              </FormControl>
+            </Grid>
+            {/* Network Reliability
             <Grid item xs={10}>
               <FormControl fullWidth variant="outlined">
                 <InputLabel id="network-reliability-label">Network Reliability</InputLabel>
@@ -165,28 +206,27 @@ const BasicForm = () => {
                 )}
               </FormControl>
             </Grid>
-
-            {/* Data Storage Size */}
+ */}
+            {/* Data Storage Type */}
             <Grid item xs={10}>
               <FormControl fullWidth variant="outlined">
-                <InputLabel id="data-storage-size-label">Data Storage Size</InputLabel>
+                <InputLabel id="data-storage-type-label">What type of data do you work with ?</InputLabel>
                 <Select
-                  labelId="data-storage-size-label"
-                  id="data-storage-size-select"
-                  value={formData.dataStorageSize}
+                  labelId="data-storage-type-label"
+                  id="data-storage-type-select"
+                  value={formData.dataStorageType}
                   onChange={handleChange}
-                  label="Data Storage Size"
-                  name="dataStorageSize"
-                  error={validationErrors.dataStorageSize}
+                  label="Data Storage Type"
+                  name="dataStorageType"
+                  error={validationErrors.dataStorageType}
                 >
                   <MenuItem value="">Select...</MenuItem>
-                  <MenuItem value="small">Small: Up to 100GB of data storage</MenuItem>
-                  <MenuItem value="medium">Medium: 100GB to 1TB of data storage</MenuItem>
-                  <MenuItem value="large">Large: More than 1TB of data storage</MenuItem>
-                  <MenuItem value="verylarge">Very Large: More than 100 TB of data</MenuItem>
+                  <MenuItem value="files">Files</MenuItem>
+                  <MenuItem value="databases">Databases</MenuItem>
+                  <MenuItem value="multimedia">Multimedia</MenuItem>
                 </Select>
-                {validationErrors.dataStorageSize && (
-                  <FormHelperText error>Please select data storage size</FormHelperText>
+                {validationErrors.dataStorageType && (
+                  <FormHelperText error>Please select data storage type</FormHelperText>
                 )}
               </FormControl>
             </Grid>
@@ -205,15 +245,16 @@ const BasicForm = () => {
                   error={validationErrors.databaseService}
                 >
                   <MenuItem value="">Select...</MenuItem>
-                  <MenuItem value="sql">
-                    SQL: Relational database for structured data storage and querying
+                  <MenuItem value="basic">
+                    Basic Database
                   </MenuItem>
-                  <MenuItem value="nosql">
-                    NoSQL: Non-relational database for flexible data models
+                  <MenuItem value="complex">
+                    Complex Database
                   </MenuItem>
                   <MenuItem value="nodatabase">
-                    No database required: No need for a database service at the moment
+                    No database
                   </MenuItem>
+
                 </Select>
                 {validationErrors.databaseService && (
                   <FormHelperText error>Please select database service</FormHelperText>
@@ -245,8 +286,104 @@ const BasicForm = () => {
                 )}
               </FormControl>
             </Grid>
+            {/* DNS Feature */}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel id="dns-feature-label">Do you want to ensure that your website is identifiable by a user-friendly address?</InputLabel>
+                <Select
+                  labelId="dns-feature-label"
+                  id="dns-feature-select"
+                  value={formData.dnsFeature}
+                  onChange={handleChange}
+                  label="DNS Networking"
+                  name="dnsFeature"
+                  error={validationErrors.dnsFeature}
+                >
+                  <MenuItem value="">Select...</MenuItem>
+                  <MenuItem value="Yes">
+                    Yes
+                  </MenuItem>
+                  <MenuItem value="No">
+                    No
+                  </MenuItem>
+                </Select>
+                {validationErrors.dnsFeature && (
+                  <FormHelperText error>Please select your option</FormHelperText>
+                )}
+              </FormControl>
+            </Grid>
+            {/* DNS Feature */}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel id="cdn-feature-label">Do you have a website or application with global users and want to minimize delays in loading web content? </InputLabel>
+                <Select
+                  labelId="cdn-feature-label"
+                  id="cdn-feature-select"
+                  value={formData.cdnNetworking}
+                  onChange={handleChange}
+                  label="CDN Networking"
+                  name="cdnNetworking"
+                  error={validationErrors.cdnNetworking}
+                >
+                  <MenuItem value="">Select...</MenuItem>
+                  <MenuItem value="Yes">
+                    Yes
+                  </MenuItem>
+                  <MenuItem value="No">
+                    No
+                  </MenuItem>
+                </Select>
+                {validationErrors.cdnNetworking && (
+                  <FormHelperText error>Please select your option</FormHelperText>
+                )}
+              </FormControl>
+            </Grid>
+            {/* Region */}
+            <Grid item xs={10}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel id="dns-feature-label">Region</InputLabel>
+                <Select
+                  labelId="dns-feature-label"
+                  id="dns-feature-select"
+                  value={formData.region}
+                  onChange={handleChange}
+                  label="Region"
+                  name="region"
+                  error={validationErrors.region}
+                >
+                  <MenuItem value="">Select...</MenuItem>
+                  <MenuItem value="us-east-1">US East (N. Virginia)</MenuItem>
+                  <MenuItem value="us-east-2">US East (Ohio)</MenuItem>
+                  <MenuItem value="us-west-1">US West (N. California)</MenuItem>
+                  <MenuItem value="us-west-2">US West (Oregon)</MenuItem>
+                  <MenuItem value="ap-east-1">Asia Pacific (Hong Kong)</MenuItem>
+                  <MenuItem value="ap-south-1">Asia Pacific (Mumbai)</MenuItem>
+                  <MenuItem value="ap-northeast-3">Asia Pacific (Osaka-Local)</MenuItem>
+                  <MenuItem value="ap-northeast-2">Asia Pacific (Seoul)</MenuItem>
+                  <MenuItem value="ap-southeast-1">Asia Pacific (Singapore)</MenuItem>
+                  <MenuItem value="ap-southeast-2">Asia Pacific (Sydney)</MenuItem>
+                  <MenuItem value="ap-northeast-1">Asia Pacific (Tokyo)</MenuItem>
+                  <MenuItem value="ca-central-1">Canada (Central)</MenuItem>
+                  <MenuItem value="cn-north-1">China (Beijing)</MenuItem>
+                  <MenuItem value="cn-northwest-1">China (Ningxia)</MenuItem>
+                  <MenuItem value="eu-central-1">EU (Frankfurt)</MenuItem>
+                  <MenuItem value="eu-west-1">EU (Ireland)</MenuItem>
+                  <MenuItem value="eu-west-2">EU (London)</MenuItem>
+                  <MenuItem value="eu-south-1">EU (Milan)</MenuItem>
+                  <MenuItem value="eu-west-3">EU (Paris)</MenuItem>
+                  <MenuItem value="eu-north-1">EU (Stockholm)</MenuItem>
+                  <MenuItem value="me-south-1">Middle East (Bahrain)</MenuItem>
+                  <MenuItem value="sa-east-1">South America (Sao Paulo)</MenuItem>
+                  <MenuItem value="af-south-1">Africa (Cape Town)</MenuItem>
+                </Select>
+                {validationErrors.region && (
+                  <FormHelperText error>Please select your region</FormHelperText>
+                )}
+              </FormControl>
+            </Grid>
 
-            {/* Resource Growth */}
+
+            {/* Resource Growth
             <Grid item xs={10}>
               <FormControl fullWidth variant="outlined">
                 <InputLabel id="resource-growth-label">Resource Growth</InputLabel>
@@ -268,7 +405,7 @@ const BasicForm = () => {
                   <FormHelperText error>Please select resource growth</FormHelperText>
                 )}
               </FormControl>
-            </Grid>
+            </Grid> */}
 
             {/* Submit Button */}
             <Grid item xs={10}>
