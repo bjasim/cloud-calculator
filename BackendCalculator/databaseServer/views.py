9f9a8e9e9a0b3f40d62db45e4dc700445462ee35
philip.wojdyna <philip.wojdyna70@gmail.com>
2024-03-17 11:08:20 -0400

    Current  database/views.py that works with Oracle

--- BackendCalculator/databaseServer/views.py
+++ BackendCalculator/databaseServer/views.py
@@ -3,6 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 from azure_app.views import calculated_data_Azure
+from aws_app.views import calculated_data_AWS
+from google_app.views import calculated_data_Google
+from oracle_app.views import calculated_data_Oracle
+
 
 
 @csrf_exempt
@@ -10,16 +14,38 @@ def handle_advanced_form_submission(request):
     if request.method == 'POST':
         form_data = json.loads(request.body)
         print("Received form data:", form_data)
-        database_service_Azure = form_data.get('databaseService')
-        expected_cpu_Azure = form_data.get('expectedRAM')  # Assuming the CPU field stores RAM information
-        cloud_storage_Azure = form_data.get('cloudStorage')
-        networking_feature_Azure = form_data.get('networkingFeature')
+        
+        monthly_budget = form_data.get('monthlyBudget')
+        expected_cpu = form_data.get('expectedRAM')  # Assuming the CPU field stores RAM information
+        database_service = form_data.get('databaseService')
+        database_size = form_data.get('databaseSize')
+        cloud_storage = form_data.get('cloudStorage')
+        storage_size = form_data.get('storageSize')
+        dns_connection = form_data.get('dnsConnection')
+        cdn_connection = form_data.get('cdnConnection')
+        scalability = form_data.get('scalability')
+        location = form_data.get('location')
+        # networking_feature = form_data.get('networkingFeature')
 
-        calculated_data = calculated_data_Azure(database_service_Azure, expected_cpu_Azure, cloud_storage_Azure, networking_feature_Azure)
+        azure_data = calculated_data_Azure(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+        #aws_data = calculated_data_AWS(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+        google_data = calculated_data_Google(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+        Oracle_data = calculated_data_Oracle(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
 
-        print(calculated_data)
+        combined_data = {
+            'Azure': azure_data,
+            #'AWS': aws_data,
+            'Google': google_data,
+            'Oracle': Oracle_data
+        }
+
+        # print(calculated_data)
+        # # Return computed data as JSON response
+        # return JsonResponse(calculated_data)
+        print(combined_data)
         # Return computed data as JSON response
-        return JsonResponse(calculated_data)
+        return JsonResponse(combined_data)
+
     else:
         # Return HTTP 400 Bad Request for unsupported request methods
         return HttpResponseBadRequest("Unsupported request method")
@@ -29,12 +55,37 @@ def handle_advanced_form_submission(request):
 def handle_basic_form_submission(request):
     if request.method == 'POST':
         form_data = json.loads(request.body)
-
-        # Print form data to console
         print("Received form data:", form_data)
+        monthly_budget = form_data.get('monthlyBudget')
+        expected_cpu = form_data.get('computeComplexity')  # Assuming the CPU field stores RAM information
+        database_service = form_data.get('databaseService')
+        database_size = form_data.get('expectedUsers')
+        cloud_storage = form_data.get('dataStorageType')
+        storage_size = form_data.get('expectedUsers')
+        dns_connection = form_data.get('dnsFeature')
+        cdn_connection = form_data.get('cdnNetworking')
+        scalability = form_data.get('scalability')
+        location = form_data.get('region')
+        # networking_feature = form_data.get('networkingFeature')
 
-        # Return success response
-        return JsonResponse({'success': True})
+        azure_data = calculated_data_Azure(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+        #aws_data = calculated_data_AWS(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+        google_data = calculated_data_Google(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+        Oracle_data = calculated_data_Oracle(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+
+        combined_data = {
+            'Azure': azure_data,
+            #'AWS': aws_data,
+            'Google': google_data,
+            'Oracle': Oracle_data
+        }
+
+        # print(calculated_data)
+        # # Return computed data as JSON response
+        # return JsonResponse(calculated_data)
+        print(combined_data)
+        # Return computed data as JSON response
+        return JsonResponse(combined_data)
     else:
         # Return error response if the request method is not POST
         return JsonResponse({'success': False, 'error': 'Only POST requests are allowed'})
