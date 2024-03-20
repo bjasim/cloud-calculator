b6be92122e437b19472a9c74fc566c781da87d35
philip.wojdyna <philip.wojdyna70@gmail.com>
2024-03-20 13:06:03 -0400

    Adv. Form Adjusted for: No DB & No Storage Options.

--- BackendCalculator/databaseServer/views.py
+++ BackendCalculator/databaseServer/views.py
@@ -9,6 +9,46 @@ from oracle_app.views import calculated_data_Oracle
 
 
 
+# @csrf_exempt
+# def handle_advanced_form_submission(request):
+#     if request.method == 'POST':
+#         form_data = json.loads(request.body)
+#         print("Received form data:", form_data)
+        
+#         monthly_budget = form_data.get('monthlyBudget')
+#         expected_cpu = form_data.get('expectedRAM')  # Assuming the CPU field stores RAM information
+#         database_service = form_data.get('databaseService')
+#         database_size = form_data.get('databaseSize')
+#         cloud_storage = form_data.get('cloudStorage')
+#         storage_size = form_data.get('storageSize')
+#         dns_connection = form_data.get('dnsConnection')
+#         cdn_connection = form_data.get('cdnConnection')
+#         scalability = form_data.get('scalability')
+#         location = form_data.get('location')
+#         # networking_feature = form_data.get('networkingFeature')
+
+#         azure_data = calculated_data_Azure(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+#         #aws_data = calculated_data_AWS(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+#         google_data = calculated_data_Google(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+#         Oracle_data = calculated_data_Oracle(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
+
+#         combined_data = {
+#             'Azure': azure_data,
+#             #'AWS': aws_data,
+#             'Google': google_data,
+#             'Oracle': Oracle_data
+#         }
+
+#         # print(calculated_data)
+#         # # Return computed data as JSON response
+#         # return JsonResponse(calculated_data)
+#         print(combined_data)
+#         # Return computed data as JSON response
+#         return JsonResponse(combined_data)
+
+#     else:
+#         # Return HTTP 400 Bad Request for unsupported request methods
+#         return HttpResponseBadRequest("Unsupported request method")
 @csrf_exempt
 def handle_advanced_form_submission(request):
     if request.method == 'POST':
@@ -16,32 +56,27 @@ def handle_advanced_form_submission(request):
         print("Received form data:", form_data)
         
         monthly_budget = form_data.get('monthlyBudget')
-        expected_cpu = form_data.get('expectedRAM')  # Assuming the CPU field stores RAM information
+        expected_cpu = form_data.get('expectedRAM')
         database_service = form_data.get('databaseService')
-        database_size = form_data.get('databaseSize')
+        database_size = form_data.get('databaseSize') if form_data.get('databaseService') != "noDatabase" else None
         cloud_storage = form_data.get('cloudStorage')
-        storage_size = form_data.get('storageSize')
+        storage_size = form_data.get('storageSize') if form_data.get('cloudStorage') != "No Storage" else None
         dns_connection = form_data.get('dnsConnection')
         cdn_connection = form_data.get('cdnConnection')
         scalability = form_data.get('scalability')
         location = form_data.get('location')
-        # networking_feature = form_data.get('networkingFeature')
 
+        # Perform calculations (Example functions need to be defined by you)
         azure_data = calculated_data_Azure(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
-        #aws_data = calculated_data_AWS(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
         google_data = calculated_data_Google(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
         Oracle_data = calculated_data_Oracle(monthly_budget, expected_cpu, database_service, database_size, cloud_storage, storage_size, dns_connection, cdn_connection, scalability, location)
 
         combined_data = {
             'Azure': azure_data,
-            #'AWS': aws_data,
             'Google': google_data,
             'Oracle': Oracle_data
         }
 
-        # print(calculated_data)
-        # # Return computed data as JSON response
-        # return JsonResponse(calculated_data)
         print(combined_data)
         # Return computed data as JSON response
         return JsonResponse(combined_data)
